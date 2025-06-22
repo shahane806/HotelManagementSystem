8d893ae6200e56ccf98595b51ad07548165677d3
Om Shahane <om.p.shahane@gmail.com>
2025-06-22 17:10:41 +0530

    cc

---
+++ frontend/lib/myapp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/Ui/splash_screen.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ frontend/lib/widgets/wrapper.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Widget wrapper(Widget child){
+  return child;
+}
