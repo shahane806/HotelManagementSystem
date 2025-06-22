afa8f3bf161ab93bc9ff9717c8f671e45b0c9677
Om Shahane <om.p.shahane@gmail.com>
2025-06-22 16:43:08 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+const rateLimit = require("express-rate-limit")
+const mongoose = require("mongoose")
+
+dotenv.config()
+const helmet = require("helmet")
+const app = express()
+app.use(helmet())
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({
+    extended:true,
+    limit:"100kb"
+}))
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
+
+const port = process.env.PORT || 5000
+const mongoDbUrl = process.env.MONGODB_URL_AUTH 
+app.listen(port,()=>{
+   console.log('App is running on port :'+port)
+})
+
+mongoose.connect(mongoDbUrl).then(()=>{
+    console.log("MongoDbAuthCluster is connected")
+})
