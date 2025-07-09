f28cd92bf0c68f5614f5b41da3c90afe7a39d8ff
Om Shahane <om.p.shahane@gmail.com>
2025-07-09 20:01:06 +0530

    commit

--- frontend/lib/app/api_constants.dart
+++ frontend/lib/app/api_constants.dart
@@ -1,3 +1,4 @@
 class ApiConstants {
-  static String url = "http://192.168.137.1:3001";
+  // static String url = "http://192.168.137.1:3001";
+  static String url = "https://hotelmanagementsystem-ysx7.onrender.com";
 }
