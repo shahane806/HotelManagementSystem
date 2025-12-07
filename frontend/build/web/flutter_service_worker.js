71a675567131e7e8789aa86c2fcd35837de30b52
Om Shahane <om.p.shahane@gmail.com>
2025-12-07 14:01:37 +0530

    c

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "acabb545d314cc5bc8e0d4df005f2107
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "bf7a3620efd6bfc5e6875fe2c8012065",
+"flutter_bootstrap.js": "511f799efcbde01438384dd407212f00",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
