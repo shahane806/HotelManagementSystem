68b0fdfa6527ed1993b8a337bdd45e339920fc87
Om Shahane <om.p.shahane@gmail.com>
2025-12-07 16:41:21 +0530

    gmail

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -7,8 +7,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 443,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
